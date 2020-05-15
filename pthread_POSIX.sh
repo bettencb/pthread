c5a1b25cbb07b07bf815eab76fb2366a0efc4772
Blaine Bettencourt <bettencb@eos.beocat.ksu.edu>
2020-05-15 17:02:13 -0500

    shell file launch issue - unable to sbatch

--- pthread_POSIX.sh
+++ pthread_POSIX.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 # NOTE the -l flag!
 #
-#SBATCH -J test
-#SBATCH -o test."%j".out
-#SBATCH -e test."%j".err
+#SBATCH -J pthread_main
+#SBATCH -o pthread_main."%j".out
+#SBATCH -e pthread_main."%j".err
 # Default in slurm
 #SBATCH --mail-user $bettencb@ksu.edu
 #SBATCH --mail-type=ALL
